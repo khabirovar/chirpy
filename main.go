fc1db6b9b76e754d9661edfe4dffceb4cac1703e
Aidar Khabirov <khabirov.ar1990@gmail.com>
2026-02-27 16:11:48 +0500

    Add healthz endpoint

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 	"log"
+	"fmt"
 )
 
 func main() {
@@ -13,7 +14,12 @@ func main() {
 		Addr: ":8080",
 	}
 	
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request){
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintf(w, "OK")
+	})
 
 	log.Fatal(server.ListenAndServe())
 }

9c3816dcbf9a6c9fe99d66eb1c79b233d6efbba6
khabirovar <khabirov.ar1990@gmail.com>
2026-02-28 11:37:37 +0500

    Rework endpoints

--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"net/http"
-	"log"
 	"fmt"
+	"log"
+	"net/http"
 	"sync/atomic"
 )
 
@@ -13,34 +13,42 @@ type apiConfig struct {
 
 func main() {
 	mux := http.NewServeMux()
-	
+
 	server := http.Server{
 		Handler: mux,
-		Addr: ":8080",
+		Addr:    ":8080",
 	}
-	
+
 	apiCfg := apiConfig{}
 
 	mux.Handle("/app/", http.StripPrefix("/app/", apiCfg.middlewareMetrics(http.FileServer(http.Dir(".")))))
 
-	mux.HandleFunc("GET /healthz", func(w http.ResponseWriter, r *http.Request){
+	mux.HandleFunc("GET /api/healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("OK"))
 	})
 
-	mux.HandleFunc("GET /metrics", func(w http.ResponseWriter, r *http.Request){
-		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	metricsPage := `
+<html>
+  <body>
+    <h1>Welcome, Chirpy Admin</h1>
+    <p>Chirpy has been visited %d times!</p>
+  </body>
+</html>
+`
+	mux.HandleFunc("GET /admin/metrics", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
 		w.WriteHeader(http.StatusOK)
-		msg := fmt.Sprintf("Hits: %d", apiCfg.fileserverHits.Load())
+		msg := fmt.Sprintf(metricsPage, apiCfg.fileserverHits.Load())
 		w.Write([]byte(msg))
 	})
 
-	mux.HandleFunc("POST /reset", func(w http.ResponseWriter, r *http.Request){
+	mux.HandleFunc("POST /admin/reset", func(w http.ResponseWriter, r *http.Request) {
 		apiCfg.fileserverHits.Swap(int32(0))
-		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
 		w.WriteHeader(http.StatusOK)
-		msg := fmt.Sprintf("Hits: %d", apiCfg.fileserverHits.Load())
+		msg := fmt.Sprintf(metricsPage, apiCfg.fileserverHits.Load())
 		w.Write([]byte(msg))
 	})
 
